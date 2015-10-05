598599fedcaf692d835fe0d8184f70e9eab98d80
SAP <shevtsov.ap@gmail.com>
2015-10-05 07:35:39 -0700

    Human, Children. Basics.

--- MacCourse/SAPMacCourse/sources/SAPL01DataTypes/main.c
+++ MacCourse/SAPMacCourse/sources/SAPL01DataTypes/main.c
@@ -36,6 +36,7 @@ int main(int argc, const char * argv[]) {
     SAPPerformTestPrintBitsOfAnyTypeNumber();
     
     SAPPerformTestGetEndian();
+    
     return 0;
 }
 
---
+++ MacCourse/SAPMacCourse/sources/SAPL05Human/SAPChildrenObject.h
@@ -0,0 +1,17 @@
+//
+//  SAPChildren.h
+//  MacCourse
+//
+//  Created by SAP on 10/5/15.
+//  Copyright (c) 2015 Yosemite Retail. All rights reserved.
+//
+
+#ifndef __MacCourse__SAPChildren__
+#define __MacCourse__SAPChildren__
+
+//#include "SAPHumanObject.h"
+
+typedef struct SAPChildrenObject SAPChildrenObject;
+
+
+#endif /* defined(__MacCourse__SAPChildren__) */
