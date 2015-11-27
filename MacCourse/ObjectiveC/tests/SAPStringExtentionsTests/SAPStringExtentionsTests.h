f03061a0ccda1a0c66bb7d219b51f9c0833d6b2e
Student 111 <marktezza@gmail.com>
2015-11-27 19:54:17 +0200

    another  commit

---
+++ MacCourse/ObjectiveC/tests/SAPStringExtentionsTests/SAPStringExtentionsTests.h
@@ -0,0 +1,13 @@
+//
+//  SAPStringExtentionsTests.h
+//  MacCourse
+//
+//  Created by Student 111 on 11/27/15.
+//  Copyright (c) 2015 Yosemite Retail. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SAPStringExtentionsTests : NSObject
+
+@end
---
+++ MacCourse/ObjectiveC/tests/SAPStringExtentionsTests/SAPStringExtentionsTests.m
@@ -0,0 +1,13 @@
+//
+//  SAPStringExtentionsTests.m
+//  MacCourse
+//
+//  Created by Student 111 on 11/27/15.
+//  Copyright (c) 2015 Yosemite Retail. All rights reserved.
+//
+
+#import "SAPStringExtentionsTests.h"
+
+@implementation SAPStringExtentionsTests
+
+@end
